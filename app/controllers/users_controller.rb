3692825b2930d42c3d67559fabb2cae27beae93b
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-03-12 16:10:02 -0700

    Made user model more secure.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,28 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?
+      "Logged In"
+    else
+      erb :'users/signup'
+    end
+  end
+
+
+
+
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def username_available
+
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates_presence_of :username, :email, :password
+  validates :username, :presence => true, :uniqueness => true, length: { minimum: 4 }
+  validates :email, :presence => true, :uniqueness => true, format: /@/
+  validates :password, length: { minimum: 6 }
 
   has_many :resolutions
   has_many :events
