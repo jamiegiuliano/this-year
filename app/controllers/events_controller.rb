6f09bd616697825d724ca3c01a1b7ba808b0756c
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-03-13 14:54:59 -0700

    Added basic routes to events controller. Fixed issue with even descriptions not updating in edit.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,9 +15,48 @@ class EventsController < ApplicationController
   post '/events/show' do
     if logged_in? && Event.create(params).valid?
       current_user.events.create(params)
+      redirect "/events"
     else
+      flash[:message] = "Event must have Title and Date."
       erb :'events/new'
     end
+  end
+
+  get '/events/:id' do
+    @event = Event.find_by_id(params[:id])
+    if logged_in? && current_user.events.include?(@event)
+      erb :'events/show'
+    else
+      redirect "/users/#{current_user.slug}"
+    end
+  end
+
+  get '/events/:id/edit' do
+    @event = Event.find_by_id(params[:id])
+    if logged_in? && current_user.events.include?(@event)
+      erb :'events/edit'
+    else
+      redirect "/users/#{current_user.slug}"
+    end
+  end
+
+  patch '/events/:id' do
+    if !logged_in?
+      redirect "/login"
+    else
+      @event = Event.find_by_id(params[:id])
+      params["event"].each do |key, value|
+          @event.update({key => value}) unless value.empty?
+      end
+      redirect "/events/#{@event.id}"
+    end
+  end
+
+  delete '/events/:id/delete' do
+    @event = Event.find_by_id(params[:id])
+
+    if @event.user == current_user then @event.delete else redirect "/users/#{@event.user.slug}" end
+
     redirect "/events"
   end
 
