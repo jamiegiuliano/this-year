0bc2010051b69810312666b55cce261bf0f0949c
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-03-17 16:55:51 -0700

    Preparing environment for Heroku.

--- config/environment.rb
+++ config/environment.rb
@@ -1,28 +1,26 @@
-ENV['SINATRA_ENV'] ||= "development"
-
 require 'bundler/setup'
-
-environments = [:default] << ENV['SINATRA_ENV'] if ENV['SINATRA_ENV']
-Bundler.require(*environments)
+Bundler.require
 
 configure :development do
+  ENV['SINATRA_ENV'] ||= "development"
+
   ActiveRecord::Base.establish_connection(
     :adapter => "sqlite3",
-    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+    :database => "db/neighborhood#{ENV['SINATRA_ENV']}.sqlite"
   )
 end
 
 configure :production do
-	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
-	ActiveRecord::Base.establish_connection(
-			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-			:host     => db.host,
-			:username => db.user,
-			:password => db.password,
-			:database => db.path[1..-1],
-			:encoding => 'utf8'
-	)
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
 end
 
 require 'rack-flash'
