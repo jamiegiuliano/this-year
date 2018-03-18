f20d9dd6d2ac16fcac9b3404c7f667bc9374f1d5
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-03-17 18:12:20 -0700

    Updating environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,17 @@
+ENV['SINATRA_ENV'] ||= "development"
+
 require 'bundler/setup'
-Bundler.require
+
+environments = [:default] << ENV['SINATRA_ENV'] if ENV['SINATRA_ENV']
+Bundler.require(*environments)
 
 configure :development do
-  ENV['SINATRA_ENV'] ||= "development"
-
   ActiveRecord::Base.establish_connection(
     :adapter => "sqlite3",
     :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
   )
 end
+
 
 configure :production do
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
