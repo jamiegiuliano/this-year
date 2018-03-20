3404b62ea4ca784d3ed3c7493c80392fed5d832a
Jamie Giuliano <jamiepgiuliano@gmail.com>
2018-03-20 10:24:33 -0700

    Adding form for Login/Signup on landing page.

--- public/js/bootstrap.js
+++ public/js/bootstrap.js
@@ -3,6 +3,27 @@
  * Copyright 2011-2016 Twitter, Inc.
  * Licensed under the MIT license
  */
+ $(function() {
+
+     $('#login-form-link').click(function(e) {
+ 		$("#login-form").delay(100).fadeIn(100);
+  		$("#register-form").fadeOut(100);
+ 		$('#register-form-link').removeClass('active');
+ 		$(this).addClass('active');
+ 		e.preventDefault();
+ 	});
+ 	$('#register-form-link').click(function(e) {
+ 		$("#register-form").delay(100).fadeIn(100);
+  		$("#login-form").fadeOut(100);
+ 		$('#login-form-link').removeClass('active');
+ 		$(this).addClass('active');
+ 		e.preventDefault();
+ 	});
+
+ });
+
+
+
  $(function () {
      $('.list-group.checked-list-box .list-group-item').each(function () {
 
